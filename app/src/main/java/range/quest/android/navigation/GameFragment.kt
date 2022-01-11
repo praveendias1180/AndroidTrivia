7502b1fdb484905f21cd987f781bfe4efec9c641
Praveen Dias <praveendias1180@gmail.com>
2022-01-11 22:23:05 +0530

    Conditional navigation implemented

--- app/src/main/java/range/quest/android/navigation/GameFragment.kt
+++ app/src/main/java/range/quest/android/navigation/GameFragment.kt
@@ -23,6 +23,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.R
 import com.example.android.navigation.databinding.FragmentGameBinding
 
@@ -97,9 +98,11 @@ class GameFragment : Fragment() {
                         setQuestion()
                         binding.invalidateAll()
                     } else {
+                        view.findNavController().navigate(R.id.action_gameFragment_to_gameWonFragment)
                         // We've won!  Navigate to the gameWonFragment.
                     }
                 } else {
+                    view.findNavController().navigate(R.id.action_gameFragment_to_gameOverFragment)
                     // Game over! A wrong answer sends us to the gameOverFragment.
                 }
             }
